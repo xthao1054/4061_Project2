359676be2368151ab5aa36b38b967d21b26fa8f0
root <pang0109@umn.edu>
2025-02-26 00:54:55 +0000

    Hi

--- swish.c
+++ swish.c
@@ -14,6 +14,8 @@
 #define CMD_LEN 512
 #define PROMPT "@> "
 
+// H
+
 int main(int argc, char **argv) {
     // Task 4: Set up shell to ignore SIGTTIN, SIGTTOU when put in background
     // You should adapt this code for use in run_command().
